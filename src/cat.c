521d69c1953ece1d9c0c00cfe6cacd921bbe333c
note9 <heemanshbhawsar2004@gmail.com>
2026-09-07 18:35:12 +0530

    cat done

--- src/cat.c
+++ src/cat.c
@@ -2,15 +2,37 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-  int fd = open("./test.txt", O_RDONLY);
+  int fd = open(argv[1], O_RDONLY);
+  if (fd == -1)
+  {
+    perror("Error opening file");
+    return 1;
+  }
   char buffer[1024];
-  ssize_t n = read(fd, buffer, sizeof(buffer));
-  printf("read %zd bytes\n", n);
-  write(STDOUT_FILENO, buffer, n);
+  ssize_t bytes_read; 
+  while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
+  {
+    ssize_t bytes_written = 0;
+    while (bytes_written < bytes_read)
+    {
+      ssize_t n = write(STDOUT_FILENO, buffer + bytes_written, bytes_read - bytes_written);
+      if (n < 0)
+      {
+        perror("write");
+        return 1;
+      }
+      bytes_written += n;
+    }
+  }
+  if (bytes_read < 0)
+  {
+    perror("read");
+    close(fd);
+    return 1;
+  }
   close(fd);
 
-  printf("fd = %d\n", fd);
   return 0;
 }
