ba54b291ef7b44f71e245420e7ffcb35c8b5ef20
note9 <heemanshbhawsar2004@gmail.com>
2026-09-08 18:26:20 +0530

    wc command done

--- src/wc.c
+++ src/wc.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[])
 {
@@ -17,18 +18,39 @@ int main(int argc, char *argv[])
   }
   char buffer[1024];
   size_t lines = 0;
+  size_t words = 0;
   size_t bytes = 0;
   ssize_t bytes_read = 0;
+  int in_word = 0;
   while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
   {
     bytes += bytes_read;
     for (int i = 0; i < bytes_read; i++)
     {
+      if (isspace((unsigned char)buffer[i]))
+      {
+        if (in_word == 1)
+        {
+          in_word = 0;
+          words++;
+        }
+      }
+      else
+      {
+        if (in_word == 0)
+        {
+          in_word = 1;
+        }
+      }
       if (buffer[i] == '\n')
       {
         lines++;
       }
     }
+  }
+  if (in_word == 1)
+  {
+    words++;
   }
   if (bytes_read < 0)
   {
@@ -39,6 +61,7 @@ int main(int argc, char *argv[])
   close(fd);
   printf("%zu\n", bytes);
   printf("%zu\n", lines);
+  printf("%zu\n", words);
 
   return 0;
 }
